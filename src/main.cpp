4b4ca3bf8a20980fedb38e97f85b14790c1efb10
Rafael Rosales <rafaelr1995@gmail.com>
2019-03-26 21:46:21 -0400

    Took away the unnesseary(cant soeel that) printf() functions and left the neccessary getters.. Next we need to add the file writing function.

--- src/main.cpp
+++ src/main.cpp
@@ -16,8 +16,6 @@ int main(void) {
     // Key/value pairs for parsing json
     json_keyval om[20];                                                         // JSON key/value pairs for MAL system info
 
-    printf("Hello World\n");
-
     c = start_data_service();
 
     while ( c < 0 ) {
@@ -40,23 +38,10 @@ int main(void) {
     mySystem.imei      = getIMEI(om, sizeof(om));
     mySystem.imsi      = getIMSI(om, sizeof(om));
 
-    printf("System Information \n");
-    printf("==============================================================\n");
-    printf("WNC: Module   #    = %s\n", mySystem.model.c_str());
-    printf("WNC: Apps Ver #    = %s\n", mySystem.appsVer.c_str());
-    printf("WNC: Firmware #    = %s\n", mySystem.firmVer.c_str());
-    printf("WNC: MAL Ver. #    = %s\n", mySystem.malwarVer.c_str());
-    printf("WNC: IP Addr. #    = %s\n", mySystem.ip.c_str());
-    printf("SIM: ICCID    #    = %s\n", mySystem.iccid.c_str());
-    printf("SIM: IMEA     #    = %s\n", mySystem.imei.c_str());
-    printf("SIM: IMSI     #    = %s\n\n",mySystem.imsi.c_str());
-
-    printf("\nNetwork Time = %s\n\n", get_networkTime(om, sizeof(om)));
+    printf("\nNetwork Time = %s\n", get_networkTime(om, sizeof(om)));
 
 
     WWANStatus(&myWwan, &myNetw);
-
-    printf("Radio mode: %s\n\n", myWwan.radioMode.c_str());
 
     printf("connType %s\n", myNetw.connType.c_str());
     printf("connState %s\n", myNetw.connState.c_str());
@@ -69,6 +54,7 @@ int main(void) {
     printf("sigLevel %s\n", myNetw.sigLevel.c_str());
     printf("lte %s\n", myNetw.lte.c_str());
     printf("wcdma %s\n", myNetw.wcdma.c_str());
+    printf("IPv4 %s\n", mySystem.ip.c_str());
     printf("ipv6 %s\n\n", myNetw.ipv6.c_str());
 
     return 0;
@@ -76,18 +62,16 @@ int main(void) {
 
 /* =====================================================================
    Copyright (c) 2018, AT&T (R)
-
    www.att.com 
  
    Licensed under the Apache License, Version 2.0 (the "License"); 
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
    Unless required by applicable law or agreed to in writing, 
    software distributed under the License is distributed on an 
    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
    either express or implied. See the License for the specific 
    language governing permissions and limitations under the License.
 ======================================================================== */
+
