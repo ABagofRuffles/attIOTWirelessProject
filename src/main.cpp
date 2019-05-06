a0fbd285fa851b777fa07b8f9eb92dd030f8eb55
Rafael Rosales <rafaelr1995@gmail.com>
2019-05-05 21:49:00 -0400

    Final update with data collection

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdio.h>
 #include "mal.hpp"
 #include "iot_monitor.h"
@@ -5,7 +6,9 @@
 #include "wwan_status.hpp"
 #include <fstream>
 #include <string>
-																// Added to create and edit text file
+#include <hwlib/hwlib.h>                                                        // Needed for WNCSDK GPIO API
+
+#define USER_BUTTON     GPIO_PIN_98
 
 using namespace std;
 
@@ -17,12 +20,12 @@ unsigned int dbg_flag = 0;
 
 void writetofile(string data, string filename) {
 	char* writable = new char[data.size() + 1];		// creates char array
-	copy(data.begin(), data.end(), writable);			// copies string to char array
-	writable[data.size()] = '\0';						// deletes the terminating 0
-	ofstream rfile(filename.c_str(), ios::app);					// sets to append to file and opens file
-	rfile.write(writable, data.size());					// adds string data to end of file
-	rfile.close();										// closes file
-	delete[] writable;									// frees the string after using it
+	copy(data.begin(), data.end(), writable);		// copies string to char array
+	writable[data.size()] = '\0';				// deletes the terminating 0
+	ofstream rfile(filename.c_str(), ios::app);		// sets to append to file and opens file
+	rfile.write(writable, data.size());			// adds string data to end of file
+	rfile.close();						// closes file
+	delete[] writable;					// frees the string after using it				
 }
 
 int main(void) {
@@ -45,8 +48,12 @@ int main(void) {
         mySystem.model=getModelID(om, sizeof(om));
     while( mySystem.model == "service is not ready");
 
+    string header = "Connection Time,Signal Strength,Signal Level,LTE RSRP,WCDMA RSCP,IPv4,IPv6\n\r";
+    writetofile(header, "data/MonitorData.csv");
+
     string info;
-    for(int  i = 0; i <3; i++) {
+
+    for(int  i = 0; i < 100000000; i++) {
     mySystem.firmVer   = getFirmwareVersion(om, sizeof(om));
     mySystem.appsVer   = getAppsVersion(om, sizeof(om));
     mySystem.malwarVer = getMALManVer(om, sizeof(om));
@@ -74,37 +81,38 @@ int main(void) {
     printf("IPv4 %s\n", mySystem.ip.c_str());
     printf("ipv6 %s\n\n", myNetw.ipv6.c_str());
 	
-	//Write data to text file
+	// Write data to text file
 
-	string info(myNetw.connType.c_str());
-	info += "\n\r";
-	info += myNetw.connState.c_str();
-	info += "\n\r";
-	info += myNetw.connTime.c_str();
-	info += "\n\r";
-	info += myNetw.provider.c_str();
-	info += "\n\r";
-	info += myNetw.radioMode.c_str();
-	info += "\n\r";
-	info += myNetw.dbTech.c_str();
-	info += "\n\r";
-	info += myNetw.roamStatus.c_str();
-	info += "\n\r";
+	string info(myNetw.connTime.c_str());
+	info += ",";
+	// info += myNetw.connState.c_str();
+	// info += ",";
+	// info += myNetw.connTime.c_str();
+	// info += ",";
+	// info += myNetw.provider.c_str();
+	// info += ",";
+	// info += myNetw.radioMode.c_str();
+	// info += ",";
+	// info += myNetw.dbTech.c_str();
+	// info += ",";
+	// info += myNetw.roamStatus.c_str();
+	// info += ",";
 	info += myNetw.sigStrength.c_str();
-	info += "\n\r";
+	info += ",";
 	info += myNetw.sigLevel.c_str();
-	info += "\n\r";
+	info += ",";
 	info += myNetw.lte.c_str();
-	info += "\n\r";
+	info += ",";
 	info += myNetw.wcdma.c_str();
-	info += "\n\r";
+	info += ",";
 	info += mySystem.ip.c_str();
-	info += "\n\r";
+	info += ",";
 	info += myNetw.ipv6.c_str();
-	info += "\n\n\r";
-	writetofile(info, "data/MonitorData.txt");
+	info += "\n\r";
+	writetofile(info, "data/MonitorData.csv");
+        
 
-
+	sleep(2);
     }
 
     return 0;
